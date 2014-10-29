dbc6ec944bccbde8e1a5682d14ce3ab354ec207e
MLackner <lacknersmichael@gmail.com>
2014-10-29 11:50:45 +0100

    Changed: Gauss function to original (ref: Tyrode)

--- fcn_gauss.m
+++ fcn_gauss.m
@@ -5,18 +5,17 @@ function thetaAvg = fcn_gauss(theta,thetaMean,variance,power)
 fh = @(theta) fcn_gaussIntegral(theta,thetaMean,variance,power);
 
 % Gauss function
-% thetaAvg = 1/(variance*sqrt(2*pi)) .* integral(fh,0,pi);
+thetaAvg = 1/(variance*sqrt(2*pi)) .* integral(fh,0,pi);
 
 % thetaAvg = cos(1/(variance*sqrt(2*pi)) .* integral(fh,0,pi,'AbsTol',1e-40)).^power;
-thetaAvg = cos(1/(variance*sqrt(2*pi)) .* integral(fh,0,pi,'AbsTol',1e-40)).^power;
 end
 
 function integral = fcn_gaussIntegral(theta,thetaMean,variance,power)
-% integral = cos(theta).^(power) .* ...
-%     exp((-(theta - thetaMean).^2)./(2*variance.^2)).* ...
-%     sin(theta);
-
-integral = exp((-(theta - thetaMean).^2)./(2*variance.^2)).* ...
+integral = cos(theta).^(power) .* ...
+    exp((-(theta - thetaMean).^2)./(2*variance.^2)).* ...
     sin(theta);
+
+%integral = exp((-(theta - thetaMean).^2)./(2*variance.^2)).* ...
+%     sin(theta);
 
 end
